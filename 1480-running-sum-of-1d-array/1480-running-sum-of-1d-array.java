4b1feccdf43a7c9253c897685d9274dcb0275332
Otra <158626103+Ulton321@users.noreply.github.com>
2024-12-21 00:01:03 +0700

    Update 1480-running-sum-of-1d-array.java
    I've added a check to handle null or empty arrays to prevent potential issues

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,7 +1,13 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        for(int i=1;i<nums.length;i++)
-           nums[i]+=nums[i-1];
+        if (nums == null || nums.length == 0) {
+            return new int[0]; 
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+
         return nums;
     }
 }
